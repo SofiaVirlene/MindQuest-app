02870dda9e1b2e86e9b329f69cc358ca27e1898e
SofiaVirlene <sofiavirlene@gmail.com>
2026-07-24 15:45:12 -0300

    feat: adiciona geracao de codigo mfa em auth.py

--- mindquest-app/auth.py
+++ mindquest-app/auth.py
@@ -1,7 +1,7 @@
 import bcrypt
+import random
 
 def gerar_hash_senha(senha: str) -> str:
-    # Trunca a senha em 72 bytes por segurança antes de gerar o hash
     senha_bytes = senha.encode('utf-8')[:72]
     salt = bcrypt.gensalt()
     hash_bytes = bcrypt.hashpw(senha_bytes, salt)
@@ -11,3 +11,8 @@ def verificar_senha(senha_digitada: str, hash_salvo: str) -> bool:
     senha_bytes = senha_digitada.encode('utf-8')[:72]
     hash_bytes = hash_salvo.encode('utf-8')
     return bcrypt.checkpw(senha_bytes, hash_bytes)
+
+def gerar_codigo_mfa(email: str):
+    codigo = f"{random.randint(100000, 999999)}"
+    print(f"Código MFA para {email}: {codigo}")
+    return codigo
